5238c053857e13d6000c181107a545a52d6beffb
SanaArshad12 <sanaarshad1209@gmail.com>
2025-01-27 22:31:56 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@google/generative-ai']
+  //   }
+  // }
+ 
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+ 
+// })
